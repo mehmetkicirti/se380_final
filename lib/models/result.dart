aeb9fda3c91b1b422d0c7f03dae54bd902519307
mehmetkicirti <mehmetkicirti@hotmail.com>
2020-07-06 01:33:28 +0300

    Unnecessary files was commented.

--- lib/models/result.dart
+++ lib/models/result.dart
@@ -35,25 +35,25 @@ class Result {
   };
 }
 
-class Dates {
-  DateTime maximum;
-  DateTime minimum;
-
-  Dates({
-    this.maximum,
-    this.minimum,
-  });
-
-  factory Dates.fromMap(Map<String, dynamic> json) => Dates(
-    maximum: DateTime.parse(json["maximum"]),
-    minimum: DateTime.parse(json["minimum"]),
-  );
-
-  Map<String, dynamic> toMap() => {
-    "maximum": "${maximum.year.toString().padLeft(4, '0')}-${maximum.month.toString().padLeft(2, '0')}-${maximum.day.toString().padLeft(2, '0')}",
-    "minimum": "${minimum.year.toString().padLeft(4, '0')}-${minimum.month.toString().padLeft(2, '0')}-${minimum.day.toString().padLeft(2, '0')}",
-  };
-}
+//class Dates {
+//  DateTime maximum;
+//  DateTime minimum;
+//
+//  Dates({
+//    this.maximum,
+//    this.minimum,
+//  });
+//
+//  factory Dates.fromMap(Map<String, dynamic> json) => Dates(
+//    maximum: DateTime.parse(json["maximum"]),
+//    minimum: DateTime.parse(json["minimum"]),
+//  );
+//
+//  Map<String, dynamic> toMap() => {
+//    "maximum": "${maximum.year.toString().padLeft(4, '0')}-${maximum.month.toString().padLeft(2, '0')}-${maximum.day.toString().padLeft(2, '0')}",
+//    "minimum": "${minimum.year.toString().padLeft(4, '0')}-${minimum.month.toString().padLeft(2, '0')}-${minimum.day.toString().padLeft(2, '0')}",
+//  };
+//}
 
 class ResultElement {
   double popularity;
